f6accea1ac38d5fa25ac73b3fa674e16c0fefbe9
Dhiraj Choudhary <dhirajch145@gmail.com>
2022-04-23 21:57:13 +0530

    slug added

---
+++ pages/product/[slug].js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductDetails = () => {
+    return (
+        <div>
+            <div className='product-detail-container'>
+                <div className='image-container'>
+                    <img src="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductDetails
